769fad2b5dd230d8da38155a7b194c2b40b90fcf
JIghtuse <jightuse@gmail.com>
2012-01-14 17:59:51 +0700

    Working version (random fill).

--- src/main.c
+++ src/main.c
@@ -2,14 +2,20 @@
 #include <stdlib.h>
 #include <ncurses.h>
 #include <time.h>
+#include <unistd.h>
 
 #define LIFE 1
 #define EMPTY 0
 #define SYMBOL '#'
 
+#define NBORN 3
+#define NMIN 2
+#define NMAX 3
+
 typedef unsigned short int *cell;
 
 void init_universe(cell c, cell newc);
+void update_universe(cell c, cell newc);
 void print_universe(cell c);
 
 int main (int argc, char *argv[])
@@ -19,14 +25,20 @@ int main (int argc, char *argv[])
 	srand(time(NULL));
 	
 	initscr();
-	printw("Hello world! cols: %d, rows: %d\n", COLS, LINES);
+	curs_set(0);
 
 	c = malloc (sizeof(cell) * COLS * LINES);
 	newc = malloc (sizeof(cell) * COLS * LINES);
 
 	init_universe(c, newc);
 	print_universe(c);
+	while (1) {
+		sleep(1);
+		print_universe(c);
+		update_universe(c, newc);
+	}
 
+	curs_set(1);
 	endwin();
 	free(c);
 	free(newc);
@@ -45,13 +57,40 @@ void init_universe(cell c, cell newc)
 
 void print_universe(cell c) {
 	int i, j;
+	clear();
+	for (i = 0; i < LINES; i++) {
+		for (j = 0; j < COLS; j++) {
+			if (c[i * COLS + j] == LIFE) {
+				mvprintw(i, j, "%c", SYMBOL);
+			}
+		}
+	}
+	refresh();
+}
+
+void update_universe(cell c, cell newc) {
+	int i, j, ni, neighs;
 
 	for (i = 0; i < LINES; i++) {
 		for (j = 0; j < COLS; j++) {
-			mvprintw(i, j, "%c", c[i * COLS + j] == LIFE ? SYMBOL : ' ');
+			neighs = 0;
+			for (ni = i - 1; ni <= i + 1; ni++) {
+				if (c[ni * COLS + (j - 1)] == LIFE) neighs++;
+				if (c[ni * COLS + (j + 1)] == LIFE) neighs++;
+			}
+			if (c[(i - 1) * COLS + j] == LIFE) neighs++;
+			if (c[(i + 1) * COLS + j] == LIFE) neighs++;
+
+			if (c[i * COLS + j] != LIFE) {
+				newc[i * COLS + j] = (neighs == NBORN) ? LIFE : EMPTY;
+			} else {
+				newc[i * COLS + j] = (neighs > NMAX || neighs < NMIN) ? EMPTY : LIFE;
+			}
 		}
 	}
-
-	refresh();
-	getch();
+	for (i = 0; i < LINES; i++) {
+		for (j = 0; j < COLS; j++) {
+			c[i * COLS + j] = newc[i * COLS + j];
+		}
+	}
 }
